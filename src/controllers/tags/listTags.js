b429c4f69e00c05c9361a586c2686d48a15e2365
AlphansoTech <chaturj@alphansotech.com>
2025-02-04 17:08:48 +0530

    tag api completed.

--- src/controllers/tags/listTags.js
+++ src/controllers/tags/listTags.js
@@ -1,29 +1,43 @@
-import Tags from '../../models/Tags.js'
-import getSortParams from '../utils/getSortParams.js'
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const tagFilePath = path.join(__dirname, 'tags.json');
+
+fs.promises.access(tagFilePath)
+  .catch(() => fs.promises.writeFile(tagFilePath, '[]'))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
 export default async function listTags(req, res, next) {
   try {
-    const { sortBy, sortOrder } = getSortParams(req.query, {
-      default: { field: 'name', order: 1 },
-      name: { field: 'name', order: 1 },
-      quoteCount: { field: 'quoteCount', order: -1 },
-      dateAdded: { field: 'dateAdded', order: -1 },
-      dateModified: { field: 'dateModified', order: -1 },
-    })
+    // const { sortBy, sortOrder } = getSortParams(req.query, {
+    //   default: { field: 'name', order: 1 },
+    //   name: { field: 'name', order: 1 },
+    //   quoteCount: { field: 'quoteCount', order: -1 },
+    //   dateAdded: { field: 'dateAdded', order: -1 },
+    //   dateModified: { field: 'dateModified', order: -1 },
+    // })
 
-    const results = await Tags.aggregate([
-      {
-        $lookup: {
-          from: 'quotes',
-          localField: 'name',
-          foreignField: 'tags',
-          as: 'quoteCount',
-        },
-      },
-      { $addFields: { quoteCount: { $size: '$quoteCount' } } },
-      { $sort: { [sortBy]: sortOrder } },
-    ])
-    res.json(results)
+    // const results = await Tags.aggregate([
+    //   {
+    //     $lookup: {
+    //       from: 'quotes',
+    //       localField: 'name',
+    //       foreignField: 'tags',
+    //       as: 'quoteCount',
+    //     },
+    //   },
+    //   { $addFields: { quoteCount: { $size: '$quoteCount' } } },
+    //   { $sort: { [sortBy]: sortOrder } },
+    // ])
+    const data = await fs.promises.readFile(tagFilePath, 'utf8');
+    const tagArray = JSON.parse(data);
+    res.json(tagArray)
   } catch (error) {
     return next(error)
   }
