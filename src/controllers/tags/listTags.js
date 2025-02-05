15b9dd45fd7229b6a160bd67ed43b5d6decbc6ca
AlphansoTech <chaturj@alphansotech.com>
2025-02-05 16:16:05 +0530

    tags sort issue fixed.

--- src/controllers/tags/listTags.js
+++ src/controllers/tags/listTags.js
@@ -16,33 +16,18 @@ fs.promises.access(tagFilePath)
 
 export default async function listTags(req, res, next) {
   try {
-    const { sortBy, sortOrder } = getSortParams(req.query, {
-      default: { field: 'name', order: 1 },
-      name: { field: 'name', order: 1 },
-      quoteCount: { field: 'quoteCount', order: -1 },
-      dateAdded: { field: 'dateAdded', order: -1 },
-      dateModified: { field: 'dateModified', order: -1 },
-    })
-
-    // const results = await Tags.aggregate([
-    //   {
-    //     $lookup: {
-    //       from: 'quotes',
-    //       localField: 'name',
-    //       foreignField: 'tags',
-    //       as: 'quoteCount',
-    //     },
-    //   },
-    //   { $addFields: { quoteCount: { $size: '$quoteCount' } } },
-    //   { $sort: { [sortBy]: sortOrder } },
-    // ])
     const tagArraydata = await fs.promises.readFile(tagFilePath, 'utf8');
-    // tagArraydata.prototype.sort.call(sortBy);
-
-    var ascendingOrder = Array.prototype.sort.call(tagArraydata,sortOrder);//sortBy(tagArraydata, sortOrder);
-    const tagArray = JSON.parse(ascendingOrder);
+    var parse = Object.values(JSON.parse(tagArraydata));
+    const tagArray = parse.sortBy('name');
     res.json(tagArray)
   } catch (error) {
     return next(error)
   }
+}
+
+
+Array.prototype.sortBy = function(p) {
+  return this.slice(0).sort(function(a,b) {
+    return (a[p] > b[p]) ? 1 : (a[p] < b[p]) ? -1 : 0;
+  });
 }
